b48ef5f7524e39b69e17329bdc20762db1139104
Minnakanti Vinay Babu <73283398+vinay-14298@users.noreply.github.com>
2022-04-14 16:15:18 +0530

    Update index.php

--- index.php
+++ index.php
@@ -106,6 +106,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
 <body>
     <div class="wrapper">
         <h2>Login</h2>
+     <h2>vinay babu</h2>
         <p>Please fill in your credentials to login.</p>
         <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
             <div class="form-group <?php echo (!empty($username_err)) ? 'has-error' : ''; ?>">
